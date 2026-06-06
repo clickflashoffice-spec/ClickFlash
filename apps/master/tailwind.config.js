dae6d2b845b32a97b90aee6410a8545c85085d89
alaeddinekhemiri <75678725+alaeddinekhemiri@users.noreply.github.com>
2026-06-06 14:12:14 +0100

    fix(master): exclude node_modules symlinks from Tailwind content scan
    Prevents infinite scanning through src/components/ui/node_modules/
    symlinks which caused production builds to hang indefinitely.
    
    Build time: 28.89s (was hanging 5+ minutes)

--- apps/master/tailwind.config.js
+++ apps/master/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
+    "!**/node_modules/**",
   ],
   darkMode: "class",
   theme: {
